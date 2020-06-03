91c0f0c994ca4f6e867712c400c0b80df8ed5456
Enrico Minack <github@enrico.minack.dev>
2020-06-03 11:36:48 +0200

    Allow to load DGraph via DataFrameReader.load(path)

--- src/main/scala/uk/co/gresearch/spark/dgraph/connector/TableProviderBase.scala
+++ src/main/scala/uk/co/gresearch/spark/dgraph/connector/TableProviderBase.scala
@@ -10,10 +10,23 @@ trait TableProviderBase extends TableProvider with DataSourceRegister {
 
   protected def getTargets(map: CaseInsensitiveStringMap): Seq[Target] = {
     val objectMapper = new ObjectMapper()
-    val targets = Option(map.get(TargetsOption)).map { pathStr =>
-      objectMapper.readValue(pathStr, classOf[Array[String]]).toSeq
-    }.getOrElse(Seq.empty)
-    (targets ++ Option(map.get(TargetOption))).map(Target)
+    val fromTargets = Seq(TargetsOption, "paths").flatMap(option =>
+      Option(map.get(option)).map { pathStr =>
+        objectMapper.readValue(pathStr, classOf[Array[String]]).toSeq
+      }.getOrElse(Seq.empty[String])
+    )
+
+    val fromTarget = Seq(TargetOption, "path").flatMap(option =>
+      Option(map.get(option))
+    )
+
+    val allTargets = fromTargets ++ fromTarget
+    if (allTargets.isEmpty)
+      throw new IllegalArgumentException("No DGraph servers provided, provide targets via " +
+        "DataFrameReader.load(…) or DataFrameReader.option(TargetOption, …)"
+      )
+
+    allTargets.map(Target)
   }
 
 }
