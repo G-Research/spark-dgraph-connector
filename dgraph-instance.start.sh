c7ae5ac34f972a729c456b4a456074ae6c797211
Enrico Minack <github@enrico.minack.dev>
2020-08-12 22:32:42 +0200

    Make unit tests work with Dgraph 20.07.0 (#47)
    Also build and test with multiple Dgraph versions.

--- dgraph-instance.start.sh
+++ dgraph-instance.start.sh
@@ -1,1 +1,4 @@
-docker run --rm -it -p 8080:8080 -p 9080:9080 -p 8000:8000 -p 6080:6080 -v "$(cd "$(dirname "$0")"; pwd)/dgraph-instance:/dgraph" dgraph/dgraph:v20.03.4 /bin/bash -c "dgraph-ratel & dgraph zero & dgraph alpha --lru_mb 1024"
+# keep env var name in-sync with uk.co.gresearch.spark.dgraph.DgraphTestCluster.DgraphVersionEnvVar
+# keep default value in-sync with uk.co.gresearch.spark.dgraph.DgraphTestCluster.DgraphDefaltVersion
+version=${DGRAPH_TEST_CLUSTER_VERSION:-20.03.4}
+docker run --rm -it -p 8080:8080 -p 9080:9080 -p 8000:8000 -p 6080:6080 -v "$(cd "$(dirname "$0")"; pwd)/dgraph-instance:/dgraph" dgraph/dgraph:v${version} /bin/bash -c "dgraph-ratel & dgraph zero & dgraph alpha --lru_mb 1024 --whitelist 0.0.0.0/0"
