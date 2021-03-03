ea5e2f058561a2dfec3b349ffca816215f91c7ae
Enrico Minack <github@enrico.minack.dev>
2021-03-03 18:28:17 +0100

    Make FilterPushdownTestHelper deterministic

--- src/test/scala/uk/co/gresearch/spark/dgraph/connector/FilterPushdownTestHelper.scala
+++ src/test/scala/uk/co/gresearch/spark/dgraph/connector/FilterPushdownTestHelper.scala
@@ -38,7 +38,7 @@ trait FilterPushdownTestHelper extends Assertions {
     val root = plan match {
       case filter: execution.FilterExec =>
         val unpushedFilters = getFilterNodes(filter.condition)
-        assert(unpushedFilters.map(_.sql) === expectedUnpushed.map(_.sql))
+        assert(unpushedFilters.map(_.sql).sorted === expectedUnpushed.map(_.sql).sorted)
         filter.child
       case _ =>
         assert(expectedUnpushed.isEmpty, "some unpushed filters expected but none filters actually unpushed")
