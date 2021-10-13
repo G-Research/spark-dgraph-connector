53df6e306f65ab8f1ce4b1ad4a569d437d8457e4
Enrico Minack <github@enrico.minack.dev>
2021-10-13 11:21:55 +0200

    Make Spark 3.2 the default branch

--- README.md
+++ README.md
@@ -62,9 +62,9 @@ The connector is under continuous development. It has the following known limita
 
 ## Using Spark Dgraph Connector
 
-The Spark Dgraph Connector is available for Spark 2.4, Spark 3.0 and Spark 3.1, all with Scala 2.12.
+The Spark Dgraph Connector is available for Spark 2.4, Spark 3.0, Spark 3.1 and Spark 3.2, all with Scala 2.12.
 Use Maven artifact ID `spark-dgraph-connector_2.12`. The Spark version is part of the package version,
-e.g. 0.7.0-2.4, 0.7.0-3.0 and  0.7.0-3.1, respectively.
+i.e. 0.7.0-2.4, 0.7.0-3.0, 0.7.0-3.1 and 0.7.0-3.2, respectively.
 Minor versions are kept in sync between those two packages,
 such that identical minor versions contain identical feature sets
 (where supported by the respective Spark version).
--- git-compare-logs-2.4.sh
+++ git-compare-logs-2.4.sh
@@ -1,11 +1,11 @@
 echo "===== missing commits ====="
-diff <(git log --pretty=format:'%s' spark-2.4..spark-3.1; echo) <(git log --pretty=format:'%s' spark-3.1..spark-2.4; echo) | grep "^[<>]" | sed -e "s/^</spark-2.4:/" -e "s/^>/spark-3.1:/"
+diff <(git log --pretty=format:'%s' spark-2.4..spark-3.2; echo) <(git log --pretty=format:'%s' spark-3.2..spark-2.4; echo) | grep "^[<>]" | sed -e "s/^</spark-2.4:/" -e "s/^>/spark-3.2:/"
 
 echo
-echo "===== spark-3.1 ====="
-git log --max-count=10 --graph --pretty=format:'%Cred%h%Creset - %s%C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative spark-2.4..spark-3.1
+echo "===== spark-3.2 ====="
+git log --max-count=10 --graph --pretty=format:'%Cred%h%Creset - %s%C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative spark-2.4..spark-3.2
 
 echo
 echo "===== spark-2.4 ====="
-git log --max-count=10 --graph --pretty=format:'%Cred%h%Creset - %s%C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative spark-3.1..spark-2.4
+git log --max-count=10 --graph --pretty=format:'%Cred%h%Creset - %s%C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative spark-3.2..spark-2.4
 
