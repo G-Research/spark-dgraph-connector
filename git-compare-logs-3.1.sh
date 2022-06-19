f1d9dfcdf6a39414b871e96fd06c9888942c6691
Enrico Minack <github@enrico.minack.dev>
2022-06-19 22:43:40 +0200

    Make Spark 3.3 the default branch

--- git-compare-logs-3.1.sh
+++ git-compare-logs-3.1.sh
@@ -1,11 +1,14 @@
+base=spark-3.3
+branch=spark-3.1
+
 echo "===== missing commits ====="
-diff <(git log --pretty=format:'%s' spark-3.1..spark-3.2; echo) <(git log --pretty=format:'%s' spark-3.2..spark-3.1; echo) | grep "^[<>]" | sed -e "s/^</spark-3.1:/" -e "s/^>/spark-3.2:/"
+diff <(git log --pretty=format:'%s' $branch..$base; echo) <(git log --pretty=format:'%s' $base..$branch; echo) | grep "^[<>]" | sed -e "s/^</$branch:/" -e "s/^>/$base:/"
 
 echo
-echo "===== spark-3.2 ====="
-git log --max-count=10 --graph --pretty=format:'%Cred%h%Creset - %s%C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative spark-3.1..spark-3.2
+echo "===== $base ====="
+git log --max-count=10 --graph --pretty=format:'%Cred%h%Creset - %s%C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative $branch..$base
 
 echo
-echo "===== spark-3.1 ====="
-git log --max-count=10 --graph --pretty=format:'%Cred%h%Creset - %s%C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative spark-3.2..spark-3.1
+echo "===== $branch ====="
+git log --max-count=10 --graph --pretty=format:'%Cred%h%Creset - %s%C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative $base..$branch
 
