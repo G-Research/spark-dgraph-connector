b5beab7bbbd388f5c96843365fbee4bcec4a1900
Enrico Minack <github@enrico.minack.dev>
2023-09-27 11:55:21 +0200

    Revert "Unify access to RowEncoder.apply and .encoderFor for testing"
    This reverts commit 12ca4b253a603164a0611f73fc75c6e8bb020757.

--- src/test/scala-spark-3.2/uk/co/gresearch/spark/package.scala
+++ src/test/scala-spark-3.2/uk/co/gresearch/spark/package.scala
@@ -1,18 +1,11 @@
 package uk.co.gresearch
 
-import org.apache.spark.sql.catalyst.encoders.RowEncoder
-import org.apache.spark.sql.{Encoder, Row}
 import org.apache.spark.sql.connector.read.InputPartition
 import org.apache.spark.sql.execution.datasources.v2.DataSourceRDDPartition
-import org.apache.spark.sql.types.StructType
 
 package object spark {
   // back porting attribute inputPartition from Spark 3.3 to make tests consistent
   implicit class ExtendedDataSourceRDDPartition(rddp: DataSourceRDDPartition) {
     def inputPartitions: Seq[InputPartition] = Seq(rddp.inputPartition)
-  }
-  // unifying access to RowEncoder.apply() and RowEncoder.encoderFor()
-  implicit class ExtendedStructType(schema: StructType) {
-    def encoder: Encoder[Row] = RowEncoder(schema)
   }
 }
--- src/test/scala-spark-3.3/uk/co/gresearch/spark/package.scala
+++ src/test/scala-spark-3.3/uk/co/gresearch/spark/package.scala
@@ -1,12 +1,3 @@
 package uk.co.gresearch
 
-import org.apache.spark.sql.{Encoder, Row}
-import org.apache.spark.sql.catalyst.encoders.RowEncoder
-import org.apache.spark.sql.types.StructType
-
-package object spark {
-  // unifying access to RowEncoder.apply() and RowEncoder.encoderFor()
-  implicit class ExtendedStructType(schema: StructType) {
-    def encoder: Encoder[Row] = RowEncoder(schema)
-  }
-}
+package object spark
