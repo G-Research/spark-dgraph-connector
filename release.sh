43c9c81516f4aa4df04cab0067cf4283297f8a37
Enrico Minack <github@enrico.minack.dev>
2024-12-16 15:17:27 +0100

    Skip tests on release versions

--- release.sh
+++ release.sh
@@ -118,17 +118,17 @@ echo
 
 # create release
 echo "Creating release packages"
-./set-version.sh 3.0.3 2.12.10; mvn clean deploy -Dsign; mvn nexus-staging:release
-./set-version.sh 3.1.3 2.12.10; mvn clean deploy -Dsign; mvn nexus-staging:release
-./set-version.sh 3.2.4 2.12.15; mvn clean deploy -Dsign; mvn nexus-staging:release
-./set-version.sh 3.3.4 2.12.15; mvn clean deploy -Dsign; mvn nexus-staging:release
-./set-version.sh 3.4.4 2.12.17; mvn clean deploy -Dsign; mvn nexus-staging:release
-./set-version.sh 3.5.3 2.12.18; mvn clean deploy -Dsign; mvn nexus-staging:release
+./set-version.sh 3.0.3 2.12.10; mvn clean deploy -Dsign -DskipTests -Dmaven.test.skip=true; mvn nexus-staging:release
+./set-version.sh 3.1.3 2.12.10; mvn clean deploy -Dsign -DskipTests -Dmaven.test.skip=true; mvn nexus-staging:release
+./set-version.sh 3.2.4 2.12.15; mvn clean deploy -Dsign -DskipTests -Dmaven.test.skip=true; mvn nexus-staging:release
+./set-version.sh 3.3.4 2.12.15; mvn clean deploy -Dsign -DskipTests -Dmaven.test.skip=true; mvn nexus-staging:release
+./set-version.sh 3.4.4 2.12.17; mvn clean deploy -Dsign -DskipTests -Dmaven.test.skip=true; mvn nexus-staging:release
+./set-version.sh 3.5.3 2.12.18; mvn clean deploy -Dsign -DskipTests -Dmaven.test.skip=true; mvn nexus-staging:release
 
-./set-version.sh 3.2.4 2.13.5; mvn clean deploy -Dsign; mvn nexus-staging:release
-./set-version.sh 3.3.4 2.13.8; mvn clean deploy -Dsign; mvn nexus-staging:release
-./set-version.sh 3.4.4 2.13.8; mvn clean deploy -Dsign; mvn nexus-staging:release
-./set-version.sh 3.5.3 2.13.8; mvn clean deploy -Dsign; mvn nexus-staging:release
+./set-version.sh 3.2.4 2.13.5; mvn clean deploy -Dsign -DskipTests -Dmaven.test.skip=true; mvn nexus-staging:release
+./set-version.sh 3.3.4 2.13.8; mvn clean deploy -Dsign -DskipTests -Dmaven.test.skip=true; mvn nexus-staging:release
+./set-version.sh 3.4.4 2.13.8; mvn clean deploy -Dsign -DskipTests -Dmaven.test.skip=true; mvn nexus-staging:release
+./set-version.sh 3.5.3 2.13.8; mvn clean deploy -Dsign -DskipTests -Dmaven.test.skip=true; mvn nexus-staging:release
 
 echo
 
