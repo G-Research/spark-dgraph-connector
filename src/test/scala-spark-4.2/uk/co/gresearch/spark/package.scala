f60476d9f793e4d2288bb7bc0335275b1ccc2571
Enrico Minack <github@enrico.minack.dev>
2026-07-31 11:50:24 +0100

    Make tests build with Spark 4.2.0

--- src/test/scala-spark-4.2/uk/co/gresearch/spark/package.scala
+++ src/test/scala-spark-4.2/uk/co/gresearch/spark/package.scala
@@ -4,8 +4,8 @@ import org.apache.spark.sql.connector.read.InputPartition
 import org.apache.spark.sql.execution.datasources.v2.DataSourceRDDPartition
 
 package object spark {
-  // back porting attribute inputPartitions from Spark 3.3 to make tests consistent
+  // recreating attribute inputPartitions removed in Spark 4.2 to make tests consistent
   implicit class ExtendedDataSourceRDDPartition(rddp: DataSourceRDDPartition) {
-    def inputPartitions: Seq[InputPartition] = Seq(rddp.inputPartition)
+    def inputPartitions: Seq[InputPartition] = rddp.inputPartition.map(Seq(_)).getOrElse(Seq.empty)
   }
 }
